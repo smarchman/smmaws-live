7b225cdd7a2815b57ac6a49ffcd78ca1e2fc2a93
Scott Marchman <scott@marchman.com>
2021-02-14 17:03:04 -0500

    Added parameter files for dev VPC

---
+++ dev/vpc/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "s3" {
+    bucket         = "smmaws-dev-terraform-state"
+    key            = "global/vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "smmaws-dev-terraform-locks"
+    encrypt        = "true"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "smmaws_vpc_dev" {
+  source = "../../../smmaws-modules/networking/vpc"
+  # VPC Info
+  vpc_cidr                 = "10.100.0.0/16"
+  vpc_enable_dns_hostnames = "true"
+  vpc_enable_dns_support   = "true"
+  vpc_name                 = "smmaws-vpc-dev"
+  vpc_owner                = "smarchman"
+  vpc_tenancy              = "default"
+  vpc_terraformed          = "yes"
+  # Public Subnet Info
+  subnet_public_1_az          = "us-east-1a"
+  subnet_public_1_cidr        = "10.100.100.0/22"
+  subnet_public_1_name        = "smmaws-public-subnet-1-dev"
+  subnet_public_1_owner       = "smarchman"
+  subnet_public_1_terraformed = "true"
+  subnet_public_2_az          = "us-east-1b"
+  subnet_public_2_cidr        = "10.100.108.0/22"
+  subnet_public_2_name        = "smmaws-public-subnet-2-dev"
+  subnet_public_2_owner       = "smarchman"
+  subnet_public_2_terraformed = "true"
+  #Private Subnet Info
+  subnet_private_1_az          = "us-east-1a"
+  subnet_private_1_cidr        = "10.100.120.0/22"
+  subnet_private_1_name        = "smmaws-private-subnet-1-dev"
+  subnet_private_1_owner       = "smarchman"
+  subnet_private_1_terraformed = "true"
+  subnet_private_2_az          = "us-east-1b"
+  subnet_private_2_cidr        = "10.100.128.0/22"
+  subnet_private_2_name        = "smmaws-private-subnet-2-dev"
+  subnet_private_2_owner       = "smarchman"
+  subnet_private_2_terraformed = "true"
+  # Internet Gateway Info
+  igw_name       = "smmaws_igw_dev"
+  rt_public_name = "smmaws_rt_public_dev"
+}
